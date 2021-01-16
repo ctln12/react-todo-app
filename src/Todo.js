79c484771cc479cfcdadd50185f15e7c8d0ec99c
Cataline Baert <snuf_kin@yahoo.com>
2021-01-16 13:09:57 +0100

    add delete a todo feature

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 class Todo extends Component {
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(evt){
+    this.props.removeTodo(this.props.id);
+  }
   render() {
     return (
       <div className="Todo">
         {this.props.content}
+        <button onClick={this.handleClick}>x</button>
       </div>
     );
   }
--- src/TodoList.js
+++ src/TodoList.js
@@ -7,18 +7,26 @@ class TodoList extends Component {
     super(props);
     this.state = { todos: [] };
     this.createTodo = this.createTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
   }
   createTodo(newTodo){
     this.setState({
       todos: [...this.state.todos, newTodo]
     });
   }
+  deleteTodo(id){
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id !== id)
+    })
+  }
   render() {
     return (
       <div className="TodoList">
         <NewTodoForm addTodo={this.createTodo} />
         <div>
-          {this.state.todos.map(todo => <Todo key={todo.id} id={todo.id} content={todo.content} />)}
+          {this.state.todos.map(todo => (
+            <Todo key={todo.id} id={todo.id} content={todo.content} removeTodo={this.deleteTodo} />
+          ))}
         </div>
       </div>
     );
