6af6b26b7a4d2fb488acc69771ecff9b0d08d474
Cataline Baert <snuf_kin@yahoo.com>
2021-01-16 10:57:41 +0100

    Move NewTodoForm and Todo components into new TotoList component and add it to App component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import './App.css';
 import React, { Component } from 'react';
-import Todo from "./Todo";
-import NewTodoForm from "./NewTodoForm";
+import TodoList from "./TodoList";
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = { todos: [] };
-    this.createTodo = this.createTodo.bind(this);
-  }
-  createTodo(newTodo){
-    this.setState({
-      todos: [...this.state.todos, newTodo]
-    });
-  }
   render() {
     return (
       <div className="App">
         <h1>React Todo App</h1>
-        <NewTodoForm addTodo={this.createTodo} />
-        <div>
-          {this.state.todos.map(todo => <Todo todo={todo} />)}
-        </div>
+        <TodoList />
       </div>
     );
   }
---
+++ src/TodoList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Todo from "./Todo";
+import NewTodoForm from "./NewTodoForm";
+
+class TodoList extends Component {
+  constructor(props){
+    super(props);
+    this.state = { todos: [] };
+    this.createTodo = this.createTodo.bind(this);
+  }
+  createTodo(newTodo){
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  }
+  render() {
+    return (
+      <div className="TodoList">
+        <NewTodoForm addTodo={this.createTodo} />
+        <div>
+          {this.state.todos.map(todo => <Todo todo={todo} />)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
