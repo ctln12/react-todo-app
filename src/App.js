7355029da0e447282540f4f71d47f602612b2d05
Cataline Baert <snuf_kin@yahoo.com>
2021-01-16 10:50:39 +0100

    create NewTodoForm component and add it to App component, modify props name of Todo component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import React, { Component } from 'react';
 import Todo from "./Todo";
+import NewTodoForm from "./NewTodoForm";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { todos: [] };
+    this.createTodo = this.createTodo.bind(this);
+  }
+  createTodo(newTodo){
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  }
   render() {
-    const content = "Do flashcards!";
     return (
       <div className="App">
         <h1>React Todo App</h1>
-        <Todo content={content} />
+        <NewTodoForm addTodo={this.createTodo} />
+        <div>
+          {this.state.todos.map(todo => <Todo todo={todo} />)}
+        </div>
       </div>
     );
   }
---
+++ src/NewTodoForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class NewTodoForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = { newTodo: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(evt){
+    this.setState({ newTodo: evt.target.value });
+  }
+  handleSubmit(evt){
+    evt.preventDefault();
+    this.props.addTodo(this.state.newTodo);
+  }
+  render() {
+    return (
+      <form className="NewTodoForm" onSubmit={this.handleSubmit}>
+        <div>
+          <label htmlFor="newTodo">New Todo</label>
+          <input
+            id="newTodo"
+            name="newTodo"
+            placeholder="New Todo"
+            onChange={this.handleChange}
+            value={this.state.newTodo}
+          />
+          <button>Add todo</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default NewTodoForm;
