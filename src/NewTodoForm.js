3d1175bc8a8aa479cda57973eed3bcb3b9ee94f8
Cataline Baert <snuf_kin@yahoo.com>
2021-01-18 15:57:17 +0100

    refactor handleChange, handleSubmit, input attributes of NewTodoForm component

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -4,26 +4,26 @@ import { v4 as uuidv4 } from 'uuid';
 class NewTodoForm extends Component {
   constructor(props){
     super(props);
-    this.state = { content: "", id: "" };
+    this.state = { content: "" };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(evt){
-    this.setState({ content: evt.target.value });
+    this.setState({ [evt.target.name]: evt.target.value });
   }
   handleSubmit(evt){
     evt.preventDefault();
-    this.props.addTodo({content: this.state.content, id: uuidv4() });
-    this.setState({ content: "", id: "" });
+    this.props.addTodo({...this.state, id: uuidv4() });
+    this.setState({ content: ""});
   }
   render() {
     return (
       <form className="NewTodoForm" onSubmit={this.handleSubmit}>
         <div>
-          <label htmlFor="newTodo">New Todo</label>
+          <label htmlFor="content">New Todo</label>
           <input
-            id="newTodo"
-            name="newTodo"
+            id="content"
+            name="content"
             placeholder="New Todo"
             onChange={this.handleChange}
             value={this.state.content}
